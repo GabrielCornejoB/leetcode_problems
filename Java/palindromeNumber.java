e6d2ada664d1faec5100e5f661bb2f0e2fb88728
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-01-29 12:05:03 -0500

    Palindrome Number (Java)

---
+++ Java/palindromeNumber.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/palindrome-number/description/
+
+package Java;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int originalNumber = x;
+        int lastDigit = x % 10;
+        int reversedNumber = 0;
+
+        if (x < 0) {
+            return false;
+        }
+
+        while(originalNumber > 0) {
+            reversedNumber = (reversedNumber * 10) + lastDigit;
+            originalNumber = originalNumber / 10;
+            lastDigit = originalNumber % 10;
+        }
+
+        return x == reversedNumber;  
+    }
+}
