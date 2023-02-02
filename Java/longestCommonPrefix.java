3a5d7f5493fa8b2d5d7ba4878a4dfffbaf3e26f2
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-02-01 21:41:55 -0500

    Longest Common Prefix (Java)

---
+++ Java/longestCommonPrefix.java
@@ -0,0 +1,19 @@
+package Java;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // Prefijo se inicializa en la primera palabra del array
+        String prefix = strs[0];
+
+        // Ciclo que recorre el array comenzando desde el segundo elemento
+        for (int i = 1; i < strs.length; i++) {
+            // Ciclo que va recortando el prefijo hasta que se encuentre en la palabra
+            // 0 -> Se encontró el prefijo en la posicicón cero, por ende, el prefijo se encuentra en la palabra
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
+
