8ce01c371cb2133d69af4705979978df2a36ce60
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-02-09 10:28:21 -0500

    Find Total Time Spent by Each Employee (MySQL)

---
+++ MySQL/findTotalTimeSpentByEachEmployee.sql
@@ -0,0 +1,2 @@
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+SELECT event_day as day, emp_id, sum(out_time - in_time) as total_time FROM Employees GROUP BY event_day, emp_id;
